901e02d287a3f0b3381cc6d686b6af0290a59ab0
yusaku-0312 <161700710+yusaku-0312@users.noreply.github.com>
2024-09-15 09:36:00 +0000

    [Finish]3-7 削除機能を作ろう

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   
-  get 'lists/new' =>'lists#new' #URLとコントローラー&アクション名が同じときはget 'lists/new'として =>意向を省略できる。
   get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  resources :lists
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
