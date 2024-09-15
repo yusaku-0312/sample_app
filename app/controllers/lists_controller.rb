a9670b75f4a963e27d012445e867fa14f09e64d5
yusaku-0312 <161700710+yusaku-0312@users.noreply.github.com>
2024-09-15 13:45:25 +0000

    [Finish]3-8 バリデーションの設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,12 +4,12 @@ class ListsController < ApplicationController
   end
   
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(list.id)
+    else
+      render :new
+    end
   end
   
   def index
@@ -29,6 +29,7 @@ class ListsController < ApplicationController
     list.update(list_params)
     redirect_to list_path(list.id)
   end
+  
   def destroy
     list = List.find(params[:id])
     list.destroy
@@ -40,5 +41,4 @@ class ListsController < ApplicationController
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
-  
 end
